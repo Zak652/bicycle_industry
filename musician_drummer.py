83f21d33e6c35b1f138dd57d16fe7cc82a56dfc7
Zak652 <m2kzak@gmail.com>
2017-08-15 03:13:14 +0300

    Band function added and tested to be working

--- musician_drummer.py
+++ musician_drummer.py
@@ -2,8 +2,9 @@ import random
 
 class Musician(object):
 	"""docstring sounds Musician"""
-	def __init__(self, sounds):
+	def __init__(self, sounds, name):
 		self.sounds = sounds
+		self.name = name
 
 
 	def solo(self, length):
@@ -13,15 +14,17 @@ class Musician(object):
 
 
 class Bassist(Musician): # The Musician class is the parent of the Bassist
-	def __init__(self):
+	def __init__(self, name):
+		self.name = name
 		# call the __init__ method of the parent class
-		super().__init__(["Twang", "Thrum", "Bling"])
+		super().__init__(["Twang", "Thrum", "Bling"], self.name)
 
 
 class Guitarist(Musician):
-	def __init__(self):
+	def __init__(self, name):
+		self.name = name
 		# call the __init__ method of the parent class
-		super().__init__(["Bonk", "Bow", "Boom"])
+		super().__init__(["Bonk", "Bow", "Boom"], self.name)
 
 
 	def tune(self):
@@ -30,9 +33,10 @@ class Guitarist(Musician):
 
 
 class Drummer(Musician):
-	def __init__(self):
+	def __init__(self, name):
+		self.name = name
 		# call the __init__ method of the parent
-		super().__init__(["Boom", "Baac", "Bang"])
+		super().__init__(["Boom", "Baac", "Bang"], self.name)
 
 	def counttofour(self):
 		for i in range(1, 5):
@@ -47,40 +51,34 @@ class Band(object):
 	"""docstring for Band"""
 	band_members = []
 
-	def __init__(self):
-		# self.name = name
-		# call the __init__ method of the parent
-		Drummer()
+	def hire(self, member):
 
-	def hire(self):
-		member = Drummer
+		self.band_members.append(member)
+		print([_member.name for _member in self.band_members])
 
-		Band.band_members.append(member)
-		print(self.band_members)
-
-
-	# def fire(self):
-	# 	Band.band_members.pop(Musician)
-	# 	print(Band.band_members)
+	def fire(self, member):
 		
+		self.band_members.remove(member)
+		print('{} has been fired from the {}'.format(member, self))
+		print([_member.name for _member in self.band_members])
+
+	def bandmusic(self):
+		
+		print (Drummer.counttofour(self))
+
+		for member in self.band_members:
+			member.solo(5)
 
 
-	# def bandplay(self):
-	# 	Drummer.counttofour()
-	# 	Drummer.solo()
-	# 	Bassist.solo()
-	# 	Guitarist.solo()
-
-
-
-# type(Band.hire)
-# print(Band.hire)
-zak = Drummer()
-nigel = Drummer()
+zak = Guitarist('Zak40')
+moses = Bassist('Moses')
+nigel = Drummer('Nigel')
 nigel.solo(9)
 print(nigel.counttofour())
 print(nigel.combust())
-Coldplay = Band()
-print(Band())
-# nigel = Coldplay.hire()
-# zak = Coldplay.hire()
+coldplay = Band()
+coldplay.hire(nigel)
+coldplay.hire(zak)
+coldplay.hire(moses)
+# coldplay.fire(nigel)
+coldplay.bandmusic()
